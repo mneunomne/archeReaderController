77ea622616fdf02e4c45d501df7326ba4c2a87cb
Alberto Harres <alberto.harres@gmail.com>
2022-06-22 10:38:52 +0200

    rename yet again

---
+++ arche-reader-controller.ino
@@ -0,0 +1,96 @@
+#define STEP_PIN_X 2
+#define DIR_PIN_X 5
+
+#define STEP_PIN_Y 3
+#define DIR_PIN_Y 6
+// 88 -> 1mm
+#define ENA_PIN 8
+
+char c;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(STEP_PIN_X,OUTPUT);
+  pinMode(DIR_PIN_X,OUTPUT);
+  pinMode(STEP_PIN_Y,OUTPUT);
+  pinMode(DIR_PIN_Y,OUTPUT);
+  pinMode(ENA_PIN,OUTPUT);
+ 
+  start();
+  
+}
+void loop() {
+  String chars = "";
+  if (Serial.available()) {
+    String s = Serial.readString();
+    // Serial.println("string: " + s);
+    // Serial.println("first char: " + s[0]);
+    int steps=0;
+    int dir=0;
+    char axis=' ';
+
+    // get direction
+    if (s[0] == '+') {
+      dir = 1;
+    } else if (s[0] == '-'){
+      dir = -1;
+    } else {
+      return;
+    }
+
+    axis = s[max(s.indexOf('y'), s.indexOf('x'))];
+    
+    // only continue if got dir value
+    if (dir != 0) {
+      String v = "";
+      for (int i = 1; i < max(s.indexOf('y'), s.indexOf('x')); i++) {
+        v += s[i];
+      }
+      steps = v.toInt();
+    }
+    
+    // move
+    if (axis == 'x') {
+      moveX(steps, dir);
+    } else if (axis == 'y') {
+      moveY(steps, dir);
+    }
+  }
+}
+
+void start () {
+  digitalWrite(ENA_PIN,LOW); // enable motor HIGH -> DISABLE
+  digitalWrite(ENA_PIN,LOW); // enable motor HIGH -> DISABLE
+
+  moveX(100, 1);
+  
+}
+
+void moveX (int steps, int dir) {
+  if (dir > 0) {
+      digitalWrite(DIR_PIN_X,LOW); // enable motor HIGH -> DISABLE
+  } else {
+      digitalWrite(DIR_PIN_X,HIGH); // enable motor HIGH -> DISABLE
+  }
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(STEP_PIN_X,HIGH);
+    delayMicroseconds(1);
+    digitalWrite(STEP_PIN_X,LOW);
+    delay(1);
+  }
+}
+
+void moveY (int steps, int dir) {
+  if (dir > 0) {
+      digitalWrite(DIR_PIN_Y,LOW); // enable motor HIGH -> DISABLE
+  } else {
+      digitalWrite(DIR_PIN_Y,HIGH); // enable motor HIGH -> DISABLE
+  }
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(STEP_PIN_Y,HIGH);
+    delayMicroseconds(1);
+    digitalWrite(STEP_PIN_Y,LOW);
+    delay(1);
+  }
+}
